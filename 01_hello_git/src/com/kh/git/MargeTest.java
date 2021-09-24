8d0f83b9cfe09af3da3bbb91cdc433320ecfc2ae
choijiwon <jiwonchoi.dev@gmail.com>
2021-09-24 12:40:37 +0900

    MergeTest base 생

---
+++ 01_hello_git/src/com/kh/git/MargeTest.java
@@ -0,0 +1,11 @@
+package com.kh.git;
+
+public class MargeTest {
+	public static void main(String[] args) {
+		System.out.println(1);
+		System.out.println(2);
+		System.out.println(3);
+		System.out.println(4);
+	}
+	
+}
